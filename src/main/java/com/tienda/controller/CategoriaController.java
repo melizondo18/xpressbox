52a429f56d19b4b3c12c6d9dc3936e1f3b7a6628
melizondo18 <melizondo10758@ufide.ac.cr>
2025-02-17 21:13:27 -0600

    Semana 4

---
+++ src/main/java/com/tienda/controller/CategoriaController.java
@@ -0,0 +1,19 @@
+/**/
+package com.tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping ("/categoria")
+public class CategoriaController {
+    
+    @GetMapping ("/listado")
+    public String listado(Model model){
+        model.addAttribute("nombre: ","Lex Voorhout");
+        
+        return "/categoria/listado";
+    }
+}
