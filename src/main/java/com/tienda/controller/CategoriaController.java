7b176710764bc763f927792e4b3291183b807e3f
melizondo18 <melizondo10758@ufide.ac.cr>
2025-02-18 20:58:02 -0600

    Configuraciones Relacionadas a la Base de Datos

--- src/main/java/com/tienda/controller/CategoriaController.java
+++ src/main/java/com/tienda/controller/CategoriaController.java
@@ -1,6 +1,8 @@
 /**/
 package com.tienda.controller;
 
+import com.tienda.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping ("/categoria")
 public class CategoriaController {
     
+    @Autowired
+    private CategoriaService categoriaService;
+    
     @GetMapping ("/listado")
     public String listado(Model model){
-        model.addAttribute("nombre: ","Lex Voorhout");
-        model.addAtribute(Mi edad es: [[${edad}]]);
+        
+        var lista = categoriaService.getCategorias(false);
+        model.addAttribute("categorias", lista);
         
         return "/categoria/listado";
     }
