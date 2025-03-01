526c2764413d819f4b9209270b9f68da7e50b198
melizondo18 <melizondo10758@ufide.ac.cr>
2025-02-28 22:03:16 -0600

    Deleted old templates, updated CategoriaController and CategoriaService

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -21,4 +21,27 @@ public class CategoriaService {
         
         return lista;
     }
+    
+    // Se escriben los metodos de CRUD Read Update Delete
+    
+    @Transactional(readOnly=true)
+    public Categoria getCategoria(Categoria categoria){
+        
+        return categoriaRepository.findById(categoria.getIdCategoria()).orElse(null);
+    }
+    
+    //Eliminar el registro del ID
+    @Transactional
+    public void delete(Categoria categoria){
+        categoriaRepository.delete(categoria);
+    }
+    
+    //Guarda el registro del ID, 
+    //Si categoria.idcategoria esta vacio se inserta un registro
+    //Si categoria.idCategoria tiene algo se modifica el registro
+    
+    @Transactional
+    public void save(Categoria categoria){
+        categoriaRepository.save(categoria);
+    }
 }
